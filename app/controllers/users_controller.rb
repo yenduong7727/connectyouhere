d1ea14c456cf77a34138ecc0c331784733b90808
kevinaditya <kevinadityauts@gmail.com>
2014-07-27 13:16:40 +1000

    Add condition to room type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to ConnectYouHere!"
       redirect_to @user
     else
       render 'new'
