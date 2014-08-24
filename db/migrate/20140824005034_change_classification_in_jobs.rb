55c8607b3246e665c19b51e14d71459aaa9e37e5
kevinaditya <kevinadityauts@gmail.com>
2014-08-24 12:25:41 +1000

    update migration

--- db/migrate/20140824005034_change_classification_in_jobs.rb
+++ db/migrate/20140824005034_change_classification_in_jobs.rb
@@ -1,6 +1,5 @@
 class ChangeClassificationInJobs < ActiveRecord::Migration
   def change
-  	alter
   	change_column :jobs, :classification, 'integer USING CAST(classification AS integer)'
   	change_table :jobs do |t|
   		t.rename :classification, :classification_id
